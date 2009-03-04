aa1c45b97e55f9b6041fa38c1abd6ec806ca1921
moro <moronatural@gmail.com>
2009-03-04 14:25:59 +0900

    fix generated test failure

--- rails-plugin/spec/gempath_spec.rb
+++ rails-plugin/spec/gempath_spec.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # vim:set fileencoding=utf-8 filetype=ruby
 require File.expand_path("spec_helper", File.dirname(__FILE__))
+require '<%= gempath %>'
 
 describe <%= gemclass %> do
   it{ <%= gemclass %>::Version.should =~ /\A\d+\.\d+\.\d+\Z/ }
